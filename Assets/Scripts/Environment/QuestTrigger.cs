33dd004c22a78d9504475668af64b3de2046bf95
Kiera Bacon <KieraJBacon@Outlook.com>
2022-04-19 19:53:15 -0400

    Added a sort of game loop?

--- Assets/Scripts/Environment/QuestTrigger.cs
+++ Assets/Scripts/Environment/QuestTrigger.cs
@@ -6,6 +6,10 @@ public class QuestTrigger : MonoBehaviour
 {
     [SerializeField]
     private QuestScriptable questItem;
+    [SerializeField]
+    private Door doorToOpen;
+    [SerializeField]
+    private bool returnToMenu;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +23,16 @@ public class QuestTrigger : MonoBehaviour
             {
                 //playerController.inventory.DeleteItem(found);
                 found.RemoveItem();
+
+                if (doorToOpen)
+                {
+                    doorToOpen.Open();
+                }
+                if (returnToMenu)
+                {
+                    AppEvents.SetMouseCursorVisible(true);
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+                }
             }
         }
     }
